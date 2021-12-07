ce6743995be0ba2f5e0eb826af08e30aadc4c91e
unknown <bomix20@gmail.com>
2021-12-07 09:53:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import photo from "./qf.jpg"
+function App() {
+  return (
+    <div className="App">
+      
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 class="title red">Your name here</h1>
+<br/>
+<img src="/ww.jpg" alt="w" />
+<br/>
+<img src={photo} alt="q"/>
+</div>
+<video width="320" height="240" controls>
+<source src="dz.mp4" type="video/mp4" />
+
+</video>
+    </div>
+  );
+}
+
+export default App;
